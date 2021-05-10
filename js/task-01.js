82f6934c55e424e90d6e1a0836d28fc1f0efc537
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-05-10 14:34:45 +0300

    1st task solution

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,11 @@
 
-const countCategoriesEl = document.querySelectorAll('#categories .item').length;
-console.log(`В списке ${countCategoriesEl} категории.`);
+const categoriesEl = document.querySelectorAll('ul#categories .item');
+
+const quantityOfCategoriesEl = categoriesEl.length;
+console.log(`В списке ${quantityOfCategoriesEl} категории.`);
+
+categoriesEl.forEach(categoryEl => {
+    console.log(`Категория: ${ (categoryEl.querySelector('h2').textContent) }`);
+    console.log(`Количество элементов: ${(categoryEl.querySelectorAll('ul li').length)}`);
+} 
+);
