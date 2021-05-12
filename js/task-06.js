8d915f534e9b8a603d6bc6ec48cf0a08b76088b4
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-05-13 01:10:38 +0300

    6th task solution

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const refs = {
+    input: document.querySelector('#validation-input'),
+    dataLength: document.querySelector('[data-length="6"]'),
+}
+
+refs.input.addEventListener('blur', onInputTest);
+const validLength = Number(refs.dataLength.dataset.length);
+
+function onInputTest(event) {
+    refs.input.classList.add('invalid');
+
+    if (event.currentTarget.value.length === validLength) {
+        refs.input.classList.replace('invalid', 'valid');
+    }
+};
