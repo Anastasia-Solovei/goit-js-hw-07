6179c9e317beca22519b466d2ae6ea56153ddc7c
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-05-18 22:25:32 +0300

    6th task solution

--- js/task-06.js
+++ js/task-06.js
@@ -9,20 +9,9 @@ const validLength = Number(refs.dataLength.dataset.length);
 function onInputTest(event) {
   refs.input.classList.add('invalid');
 
-  // заменила решение задачи на это, так как в одном из видео уроков
-  // Репета упомянул, что вызывать функции по тернарному оператору - плохая практика.
-  if (
-    event.currentTarget.value.length === validLength &&
-    refs.input.classList.contains('invalid')
-  ) {
+  if (event.currentTarget.value.length === validLength) {
     refs.input.classList.replace('invalid', 'valid');
   } else {
     refs.input.classList.replace('valid', 'invalid');
   }
-
-  // мое предыдущее решение задачи
-  //   event.currentTarget.value.length === validLength &&
-  //   refs.input.classList.contains('invalid')
-  //     ? refs.input.classList.replace('invalid', 'valid')
-  //     : refs.input.classList.replace('valid', 'invalid');
 }
