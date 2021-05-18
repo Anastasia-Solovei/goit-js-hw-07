6bb2a3715f9d31e4d0fc66fd0c91c50e387a28dd
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-05-18 09:59:15 +0300

    indicates boxSize inside the func onBoxesCreate, changes bgColor

--- js/task-08.js
+++ js/task-08.js
@@ -1,39 +1,39 @@
 const refs = {
-    input: document.querySelector('#controls input'),
-    renderBtn: document.querySelector('[data-action="render"]'),
-    destroyBtn: document.querySelector('[data-action="destroy"]'),
-    containerForBoxes: document.querySelector('#boxes'),
-}
+  input: document.querySelector('#controls input'),
+  renderBtn: document.querySelector('[data-action="render"]'),
+  destroyBtn: document.querySelector('[data-action="destroy"]'),
+  containerForBoxes: document.querySelector('#boxes'),
+};
 
 refs.renderBtn.addEventListener('click', onBoxesCreate);
 refs.destroyBtn.addEventListener('click', onBoxesDestroy);
 
-let boxSize = 30;
 function randomBgColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    const bgColor = "rgb(" + r + "," + g + "," + b + ")";
-    return bgColor;
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const bgColor = `rgb(${r},${g},${b})`;
+  return bgColor;
 }
 
 function onBoxesCreate() {
-    let ammount = Number(refs.input.value);
-    const boxes = [];
+  let ammount = Number(refs.input.value);
+  let boxSize = 30;
+  const boxes = [];
 
-    for (let i = 1; i <= ammount; i += 1) {
-        const box = document.createElement('div');
-        box.style.width = `${boxSize}px`;
-        box.style.height = `${boxSize}px`;
-        box.style.backgroundColor = randomBgColor();
+  for (let i = 1; i <= ammount; i += 1) {
+    const box = document.createElement('div');
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = randomBgColor();
 
-        boxes.push(box);
-        boxSize += 10;
-    }
+    boxes.push(box);
+    boxSize += 10;
+  }
 
-    refs.containerForBoxes.append(...boxes);
+  refs.containerForBoxes.append(...boxes);
 }
 
 function onBoxesDestroy() {
-    refs.containerForBoxes.innerHTML = '';
+  refs.containerForBoxes.innerHTML = '';
 }
